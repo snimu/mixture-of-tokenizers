ec04baf9263065cbc4d5031e426de756de23c833
snimu <sebastian.nicolas.mueller@gmail.com>
2024-12-15 22:37:35 +0100

    /// script comments for uv

--- mathblations/main.py
+++ mathblations/main.py
@@ -1,3 +1,13 @@
+# /// script
+# requires-python = "==3.12"
+# dependencies = [
+#   "torch",
+#   "wandb",
+#   "polars",
+#   "tqdm",
+#   "wandb",
+# ]
+# ///
 
 import argparse
 import itertools
