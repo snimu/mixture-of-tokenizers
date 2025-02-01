e36853a33f08857ac3e522617bc0d3811864b2f7
snimu <sebastian.nicolas.mueller@gmail.com>
2025-02-01 11:36:45 +0100

    new ablations

--- mathblations/ablations.sh
+++ mathblations/ablations.sh
@@ -27,7 +27,25 @@ uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run 
 uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 2 --savefile results --seed 67890 --num-steps 20000 --cooldown-steps 2000 --mod 23
 
 # GPU 6: addition
-uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 3 --savefile results --seed 67890 --num-steps 40000 --cooldown-steps 4000
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 3 --savefile results --seed 67890 --num-steps 20000 --cooldown-steps 2000
 
 # GPU 7: modular addition
 uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 3 --savefile results --seed 67890 --num-steps 40000 --cooldown-steps 4000 --mod 23
+
+######## WITH OUTPUT DIGITS ########
+
+# GPU 1: addition
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 2 --savefile results_out1 --seed 9876 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 1 --output-type cross_attention
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 2 --savefile results_out1 --seed 9876 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 1 --output-type sequential --mot-only  # mot-only: otherwise, baseline is just repetition of previous runs
+
+# GPUT 2: addition (large)
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 2 --savefile results_out_large1 --seed 8765 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 2 --output-type cross_attention
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 4 --max-tokens-per-num 2 --savefile results_out_large1 --seed 8765 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 2 --output-type sequential --mot-only
+
+# GPU 3: multiplication
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 3 --max-tokens-per-num 2 --savefile results_out2 --seed 7654 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 1 --output-type cross_attention --op '*'
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 3 --max-tokens-per-num 2 --savefile results_out2 --seed 7654 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 1 --output-type sequential --mot-only --op '*'
+
+# GPU 4: multiplication (large)
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 3 --max-tokens-per-num 2 --savefile results_out_large2 --seed 6543 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 2 --output-type cross_attention --op '*'
+uv run main.py --weight-decay 0.001 --num-runs 5 --regenerate-dataset-every-run --n-layer 6 --n-head 2 --n-embd 256 --use-wandb --max-digits-per-token 3 --max-tokens-per-num 2 --savefile results_out_large2 --seed 6543 --num-steps 20000 --cooldown-steps 2000 --n-layer-output 2 --output-type sequential --mot-only --op '*'
