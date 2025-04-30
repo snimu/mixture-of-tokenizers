61cfb2f4cd04fb39be7f2ec7b35bec27383bc846
snimu <sebastian.nicolas.mueller@gmail.com>
2025-04-30 21:43:08 +0200

    debug: more thorough err printing

--- scaled-pre-train/train_gpt.py
+++ scaled-pre-train/train_gpt.py
@@ -1109,7 +1109,8 @@ for step in range(train_steps + 1):
                     val_loss_fw += model(toks_in, bytes_padded_in, bytes_pulled_in, targets)
                     val_steps_fw += 1
                 except (StopIteration, RuntimeError) as e:
-                    print0(f"val_loader_fw error: {e}", console=True)
+                    import traceback
+                    print0(f"\n\nval_loader_fw error: \n\n{traceback.format_exc()}\n\n", console=True)
                     break
         val_loss_fw /= val_steps_fw
         del val_loader_fw
