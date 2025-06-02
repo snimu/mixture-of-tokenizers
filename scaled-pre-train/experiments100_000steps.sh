9196b91032ccc7731288b150cbb2c3f200fc3814
snimu <sebastian.nicolas.mueller@gmail.com>
2025-06-02 07:05:53 +0200

    train more models

--- scaled-pre-train/experiments100_000steps.sh
+++ scaled-pre-train/experiments100_000steps.sh
@@ -39,6 +39,87 @@ torchrun --nproc_per_node=8 train_gpt.py \
     --seed 773322 \
     --wandb-project MoT-scaled-pre-train-tests
 
+
+# 1.2 slightly larger byte- and token-dim
+torchrun --nproc_per_node=8 train_gpt.py \
+    --num-iterations 100000 \
+    --cooldown-frac 0.6 \
+    --seq-len 1024 \
+    --batch-size-train 64 \
+    --batch-size-val 32 \
+    --val-loss-every 1000 \
+    --save-checkpoint-every 1000 \
+    --bytes-per-token 16 \
+    --byte-mixin-method concat \
+    --byte-mixout-method noop \
+    --padding-in left \
+    --pull-in \
+    --model-dim 1024 \
+    --byte-dim 64 \
+    --token-dim 768 \
+    --seed 773322 \
+    --wandb-project MoT-scaled-pre-train-tests
+
+# 1.3 Same, but with larger byte-dim
+torchrun --nproc_per_node=8 train_gpt.py \
+    --num-iterations 100000 \
+    --cooldown-frac 0.6 \
+    --seq-len 1024 \
+    --batch-size-train 64 \
+    --batch-size-val 32 \
+    --val-loss-every 1000 \
+    --save-checkpoint-every 1000 \
+    --bytes-per-token 16 \
+    --byte-mixin-method concat \
+    --byte-mixout-method noop \
+    --padding-in left \
+    --pull-in \
+    --model-dim 1024 \
+    --byte-dim 128 \
+    --token-dim 768 \
+    --seed 773322 \
+    --wandb-project MoT-scaled-pre-train-tests
+
+# 1.4 First byte-dim but larger token-dim
+torchrun --nproc_per_node=8 train_gpt.py \
+    --num-iterations 100000 \
+    --cooldown-frac 0.6 \
+    --seq-len 1024 \
+    --batch-size-train 64 \
+    --batch-size-val 32 \
+    --val-loss-every 1000 \
+    --save-checkpoint-every 1000 \
+    --bytes-per-token 16 \
+    --byte-mixin-method concat \
+    --byte-mixout-method noop \
+    --padding-in left \
+    --pull-in \
+    --model-dim 1024 \
+    --byte-dim 64 \
+    --token-dim 896 \
+    --seed 773322 \
+    --wandb-project MoT-scaled-pre-train-tests
+
+# 1.5 Larger byte- and token-dim
+torchrun --nproc_per_node=8 train_gpt.py \
+    --num-iterations 100000 \
+    --cooldown-frac 0.6 \
+    --seq-len 1024 \
+    --batch-size-train 64 \
+    --batch-size-val 32 \
+    --val-loss-every 1000 \
+    --save-checkpoint-every 1000 \
+    --bytes-per-token 16 \
+    --byte-mixin-method concat \
+    --byte-mixout-method noop \
+    --padding-in left \
+    --pull-in \
+    --model-dim 1024 \
+    --byte-dim 128 \
+    --token-dim 896 \
+    --seed 773322 \
+    --wandb-project MoT-scaled-pre-train-tests
+
 # 2. Compare different mixout methods
 
 # 2.1 Split with 0 self-attn layers
